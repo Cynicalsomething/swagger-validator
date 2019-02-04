119ff9b14740fd108c2b59c7901c2f792baa350f
Adam Benjamin <abenjamin@gmail.com>
2019-02-03 22:09:02 -0600

    Additional work to parse swagger before validating

--- src/main/kotlin/com/madacyn/swagger/SwaggerValidator.kt
+++ src/main/kotlin/com/madacyn/swagger/SwaggerValidator.kt
@@ -1,6 +1,11 @@
 package com.madacyn.swagger
 
 import com.atlassian.oai.validator.OpenApiInteractionValidator
+import com.atlassian.oai.validator.model.SimpleRequest
+import com.atlassian.oai.validator.model.SimpleResponse
+import com.atlassian.oai.validator.report.SimpleValidationReportFormat
+import io.swagger.parser.OpenAPIParser
+import io.swagger.v3.parser.core.models.ParseOptions
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.*
@@ -24,8 +29,23 @@ class HelloController {
     fun validate(@RequestParam(value = "swaggerUrl") url: String,
                  @RequestParam(value = "path") path: String,
                  @RequestParam(value = "request") request: String,
-                 @RequestParam(value = "response") response: String) {
+                 @RequestParam(value = "response") response: String): String {
+
+        val parser = OpenAPIParser().readLocation(url, null, ParseOptions().apply {
+            isResolve = true
+            isResolveFully = true
+            isResolveCombinators = false
+        })
+
+        parser.openAPI.paths.keys.forEach { println(it) }
+
         val api = OpenApiInteractionValidator.createFor(url).build()
+        val req = SimpleRequest.Builder.post(path).withContentType("application/json").withBody(request).build()
+        val res = SimpleResponse.Builder.ok().withContentType("application/json").withBody(response).build()
+
+        val report = api.validate(req, res)
+
+        return SimpleValidationReportFormat.getInstance().apply(report)
     }
 }
 
